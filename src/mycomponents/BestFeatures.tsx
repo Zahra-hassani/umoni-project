670104656dace6c2c3737b54518ea1e0b779c650
Zahra-hassani <hassanizahra897@gmail.com>
2026-01-02 22:45:46 +0430

    working on best features of about page

---
+++ src/mycomponents/BestFeatures.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function BestFeatures() {
+  return (
+    <div className='flex flex-col p-4 items-center gap-4 bg-amber-700/15 w-full h-fit'>
+      <h1 className="font-bold font-sans text-center text-3xl">We're Solving The Biggest Problems In Furniture</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        <div className="flex flex-col items-center gap-2 p-2">
+            
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BestFeatures
